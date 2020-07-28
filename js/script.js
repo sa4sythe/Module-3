d9883c18d7949d45aedd856a4b3abaf4ad783be5
sa4sythe <67787271+sa4sythe@users.noreply.github.com>
2020-07-28 04:33:29 -0500

    added
    added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+function play() {
+    var audio = document.getElementById('audio');
+    if (audio.paused) {
+        audio.play();
+    }
+}
+function play2() {
+    var audio = document.getElementById('audio2');
+    if (audio.paused) {
+        audio.play();
+    }
+}
+function play3() {
+    var audio = document.getElementById('audio3');
+    if (audio.paused) {
+        audio.play();
+    }
+}
